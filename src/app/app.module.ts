b997677c18911ba97f1aaf15d237c6bf0266623c
NataliaVolik <log.litana@gmail.com>
2020-05-03 21:16:53 +0600

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,46 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardComponent } from './dashboard/card/card.component';
+import { FormFieldComponent } from './dashboard/form-field/form-field.component';
+import { GridListComponent } from './dashboard/grid-list/grid-list.component';
+import { AccordionComponent } from './dashboard/accordion/accordion.component';
+import { MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    CardComponent,
+    FormFieldComponent,
+    GridListComponent,
+    AccordionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatTabsModule,
+      MatCardModule,
+      MatDatepickerModule,
+      MatGridListModule,
+      MatExpansionModule,
+      MatNativeDateModule,
+      MatFormFieldModule,
+      MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/accordion/accordion.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Input} from "@angular/compiler/src/core";
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: 'accordion.component.html',
+  styleUrls: ['accordion.component.scss']
+})
+export class AccordionComponent implements OnInit {
+ panelOpenState = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
